00c43b3f242011e06df6f9f386f5a1de049b81f2
Lê Văn Triều An <52300002@student.tdtu.edu.vn>
2026-07-09 23:16:42 +0700

    chore: remove unused and deprecated files

--- fix.ps1
+++
@@ -1,5 +0,0 @@
-$path = "d:\ThapSangProject\thapsang\frontend\cohort.html"
-$content = [System.IO.File]::ReadAllText($path)
-$content = [System.Text.RegularExpressions.Regex]::Replace($content, "(?s)<<<<<<< HEAD\r?\n(.*?)\r?\n=======\r?\n.*?\r?\n>>>>>>> [^\r\n]+", "`$1")
-[System.IO.File]::WriteAllText($path, $content)
-Write-Host "Resolved conflicts in cohort.html"
